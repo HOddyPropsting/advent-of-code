4dfd465c30701bb88ad0dd0da0b85908783f8bf1
Henry Oddy-Propsting <h.oddy.propsting@gmail.com>
2020-12-20 10:25:37 +0000

    Day 7

--- Program.cs
+++ Program.cs
@@ -23,6 +23,8 @@ namespace AdventOfCode2020
             Console.WriteLine("Day 5 Part 2: {0}", Day5Part2());
             Console.WriteLine("Day 6: {0}", Day6());
             Console.WriteLine("Day 6 Part 2: {0}", Day6Part2());
+            Console.WriteLine("Day 7: {0}", Day7());
+            Console.WriteLine("Day 7 Part 2: {0}", Day7Part2());
         }
 
 
@@ -448,5 +450,86 @@ namespace AdventOfCode2020
                 return sum;
             }
         }
+
+        public static int Day7()
+        {
+            Dictionary<String, List<String>> graph = new Dictionary<string, List<string>>();
+            Regex bagRx = new Regex(@"^([a-z]* [a-z]*)");
+            Regex contentsRx = new Regex(@"([0-9]*) ([a-z]* [a-z]*) bag");
+            using (Stream stream = File.Open(@"day7", FileMode.Open))
+            using (TextReader sr = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    MatchCollection bagColour = bagRx.Matches(line);
+                    MatchCollection bagContents = contentsRx.Matches(line);
+                    
+                    foreach(Match match in bagContents)
+                    {
+                        if(graph.ContainsKey(match.Groups[2].Value))
+                        {
+                            graph[match.Groups[2].Value].Add(bagColour[0].Value);
+                        } else
+                        {
+                            List<String> contents = new List<string>();
+                            contents.Add(bagColour[0].Value);
+                            graph.Add(match.Groups[2].Value, contents);
+                        }
+                    }
+                }
+            }
+            Stack<string> containers = new Stack<string>(graph["shiny gold"]);
+            HashSet<string> tested = new HashSet<string>();
+            while(containers.Count != 0)
+            {
+                string bag = containers.Pop();
+                tested.Add(bag);
+                foreach (var outerBag in graph.GetValueOrDefault(bag, new List<string>()))
+                {
+                    if(!tested.Contains(outerBag))
+                    {
+                        containers.Push(outerBag);
+                    }
+                }
+            }
+            return tested.Count;
+        }
+        public static int Day7Part2()
+        {
+            Dictionary<String, List<(String,int)>> graph = new Dictionary<string, List<(string,int)>>();
+            int count = 0;
+            Regex bagRx = new Regex(@"^([a-z]* [a-z]*)");
+            Regex contentsRx = new Regex(@"([0-9]+) ([a-z]+ [a-z]+) bag");
+            using (Stream stream = File.Open(@"day7", FileMode.Open))
+            using (TextReader sr = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    MatchCollection bagColour = bagRx.Matches(line);
+                    MatchCollection bagContents = contentsRx.Matches(line);
+                    List<(String,int)> contents = new List<(string,int)>();
+                    foreach (Match match in bagContents)
+                    {
+                        contents.Add((match.Groups[2].Value,Int32.Parse(match.Groups[1].Value)));
+                    }
+                    graph[bagColour[0].Value] = contents;
+                }
+            }
+            Stack<(string,int)> containers = new Stack<(string,int)>(graph["shiny gold"]);
+            count += containers.Peek().Item2;
+            while (containers.Count != 0)
+            {
+                var (bag,number) = containers.Pop();
+                foreach (var (innerBag,numberOfBags) in graph.GetValueOrDefault(bag, new List<(string,int)>()))
+                {
+                    count += number * numberOfBags;
+                    containers.Push((innerBag, number * numberOfBags));
+                }
+            }
+            return count;
+        }
+
     }
 }
