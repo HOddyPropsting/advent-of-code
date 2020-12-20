d82be4dbf3ba67ff9aa4b12afd8db4e15330d8d3
Henry Oddy-Propsting <h.oddy.propsting@gmail.com>
2020-12-20 13:12:32 +0000

    day 8

--- Program.cs
+++ Program.cs
@@ -25,6 +25,8 @@ namespace AdventOfCode2020
             Console.WriteLine("Day 6 Part 2: {0}", Day6Part2());
             Console.WriteLine("Day 7: {0}", Day7());
             Console.WriteLine("Day 7 Part 2: {0}", Day7Part2());
+            Console.WriteLine("Day 8: {0}", Day8());
+            Console.WriteLine("Day 8 Part 2: {0}", Day8Part2());
         }
 
 
@@ -497,7 +499,7 @@ namespace AdventOfCode2020
         }
         public static int Day7Part2()
         {
-            Dictionary<String, List<(String,int)>> graph = new Dictionary<string, List<(string,int)>>();
+            Dictionary<String, List<(String, int)>> graph = new Dictionary<string, List<(string, int)>>();
             int count = 0;
             Regex bagRx = new Regex(@"^([a-z]* [a-z]*)");
             Regex contentsRx = new Regex(@"([0-9]+) ([a-z]+ [a-z]+) bag");
@@ -509,20 +511,20 @@ namespace AdventOfCode2020
                 {
                     MatchCollection bagColour = bagRx.Matches(line);
                     MatchCollection bagContents = contentsRx.Matches(line);
-                    List<(String,int)> contents = new List<(string,int)>();
+                    List<(String, int)> contents = new List<(string, int)>();
                     foreach (Match match in bagContents)
                     {
-                        contents.Add((match.Groups[2].Value,Int32.Parse(match.Groups[1].Value)));
+                        contents.Add((match.Groups[2].Value, Int32.Parse(match.Groups[1].Value)));
                     }
                     graph[bagColour[0].Value] = contents;
                 }
             }
-            Stack<(string,int)> containers = new Stack<(string,int)>(graph["shiny gold"]);
+            Stack<(string, int)> containers = new Stack<(string, int)>(graph["shiny gold"]);
             count += containers.Peek().Item2;
             while (containers.Count != 0)
             {
-                var (bag,number) = containers.Pop();
-                foreach (var (innerBag,numberOfBags) in graph.GetValueOrDefault(bag, new List<(string,int)>()))
+                var (bag, number) = containers.Pop();
+                foreach (var (innerBag, numberOfBags) in graph.GetValueOrDefault(bag, new List<(string, int)>()))
                 {
                     count += number * numberOfBags;
                     containers.Push((innerBag, number * numberOfBags));
@@ -531,5 +533,125 @@ namespace AdventOfCode2020
             return count;
         }
 
+        public static int Day8()
+        {
+            int acc = 0; //accumulator
+            int fp = 0;  //function pointer
+
+            List<KeyValuePair<string, int>> program = new List<KeyValuePair<string, int>>(); //list of <instruction, instruction value>
+            HashSet<int> visited = new HashSet<int>(); //we have to use a hashset as we need to know as soon as we have revisited the same instruction
+
+            using (Stream stream = File.Open(@"day8", FileMode.Open))
+            using (TextReader sr = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var parts = line.Split(" ");
+                    program.Add(new KeyValuePair<string, int>(parts[0], Int32.Parse(parts[1])));
+                }
+            }
+            while(!visited.Contains(fp))
+            {
+                visited.Add(fp);
+                switch(program[fp].Key)
+                {
+                    case "nop":
+                        fp++;
+                        break;
+                    case "acc":
+                        acc += program[fp].Value;
+                        fp++;
+                        break;
+                    case "jmp":
+                        fp += program[fp].Value;
+                        break;
+                }
+            }
+            return acc;
+        }
+
+        public static (bool,int) RunProgramToEnd(List<KeyValuePair<string, int>> program, HashSet<int> visited, int fp, int acc)
+        {
+            while(!visited.Contains(fp))
+            {
+                if(fp >= program.Count)
+                {
+                    return (true, acc);
+                }
+                visited.Add(fp);
+                switch (program[fp].Key)
+                {
+                    case "nop":
+                        fp++;
+                        break;
+                    case "acc":
+                        acc += program[fp].Value;
+                        fp++;
+                        break;
+                    case "jmp":
+                        fp += program[fp].Value;
+                        break;
+                }
+            }
+            return (false, acc);
+        }
+
+        public static int Day8Part2()
+        {
+            int acc = 0; //accumulator
+            int fp = 0;  //function pointer
+            int changed_pointer =-1;
+
+            List<KeyValuePair<string, int>> program = new List<KeyValuePair<string, int>>(); //list of <instruction, instruction value>
+            HashSet<int> visited = new HashSet<int>(); //we have to use a hashset as we need to know as soon as we have revisited the same instruction
+
+            using (Stream stream = File.Open(@"day8", FileMode.Open))
+            using (TextReader sr = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var parts = line.Split(" ");
+                    program.Add(new KeyValuePair<string, int>(parts[0], Int32.Parse(parts[1])));
+                }
+            }
+            while (true)
+            {
+                if(program[fp].Key == "nop")
+                {
+                    var clone = new List<KeyValuePair<string, int>>(program);
+                    clone[fp] = new KeyValuePair<string, int>("jmp",clone[fp].Value);
+                    var (ranToEnd, value) = RunProgramToEnd(clone, new HashSet<int>(visited), fp, acc);
+                    if (ranToEnd) return value;
+                }
+
+                if (program[fp].Key == "jmp")
+                {
+                    var clone = new List<KeyValuePair<string, int>>(program);
+                    clone[fp] = new KeyValuePair<string, int>("nop", clone[fp].Value);
+                    var (ranToEnd, value) = RunProgramToEnd(clone, new HashSet<int>(visited), fp, acc);
+                    if (ranToEnd) return value;
+                }
+                visited.Add(fp);
+
+                switch (program[fp].Key)
+                {
+                    case "nop":
+                        fp++;
+                        break;
+                    case "acc":
+                        acc += program[fp].Value;
+                        fp++;
+                        break;
+                    case "jmp":
+                        fp += program[fp].Value;
+                        break;
+                }
+            }
+        }
+
     }
+
+
 }
