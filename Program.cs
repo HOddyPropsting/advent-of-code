efa69a30d7138709eb8fbc02f50c6ca2a4e68bde
Henry Oddy-Propsting <h.oddy.propsting@gmail.com>
2020-12-20 21:28:48 +0000

    Day 11

--- Program.cs
+++ Program.cs
@@ -32,6 +32,8 @@ namespace AdventOfCode2020
             Console.WriteLine("Day 9 Part 2: {0}", Day9Part2());
             Console.WriteLine("Day 10: {0}", Day10());
             Console.WriteLine("Day 10 Part 2: {0}", Day10Part2());
+            Console.WriteLine("Day 11: {0}", Day11());
+            Console.WriteLine("Day 11 Part 2: {0}", Day11Part2());
         }
         static int Day1()
         {
@@ -812,6 +814,241 @@ namespace AdventOfCode2020
             return variants;
         }
 
+        public static char applyKernel(char[,] map, int x, int y)
+        {
+            int count=0;
+            if (map[x, y] == '.') return '.';
+
+            for(int i = x-1; i<=x+1;i++)
+            {
+                if (i < 0 || i >= map.GetLength(0)) continue;
+                for (int j = y - 1; j <= y + 1; j++)
+                {
+                    if (j < 0 || j >= map.GetLength(1)) continue;
+                    if (map[i, j] == '#') count++;
+                }
+            }
+            if(map[x,y] == '#')
+            {
+                return count >= 5 ? 'L' : '#';
+            } else
+            {
+                return count == 0 ? '#' : 'L';
+            }
+        }
+
+        public static char applyLoS(char[,] map, int x, int y)
+        {
+            int count = 0;
+            if (map[x, y] == '.') return '.';
+            for (var (i, j) = (x - 1, y - 1); i >= 0 && j >= 0; i--, j--)// - -
+            {
+                if (map[i, j] == 'L') break;
+                if (map[i, j] == '#')
+                {
+                    count++; break;
+                }
+            }
+            for (var (i, j) = (x, y - 1); j >= 0; j--)// 0 -
+            {
+                if (map[i, j] == 'L') break;
+                if (map[i, j] == '#')
+                {
+                    count++; break;
+                }
+            }
+            for (var (i, j) = (x + 1, y - 1); i < map.GetLength(0) && j >= 0; i++, j--)// + -
+            {
+                if (map[i, j] == 'L') break;
+                if (map[i, j] == '#')
+                {
+                    count++; break;
+                }
+            }
+            for (var (i, j) = (x + 1, y); i < map.GetLength(0); i++) //+ 0
+            {
+                if (map[i, j] == 'L') break;
+                if (map[i, j] == '#')
+                {
+                    count++; break;
+                }
+            }
+            for (var (i, j) = (x + 1, y + 1); i < map.GetLength(0) && j < map.GetLength(1); i++, j++)// + +
+            {
+                if (map[i, j] == 'L') break;
+                if (map[i, j] == '#')
+                {
+                    count++; break;
+                }
+            }
+            for (var (i, j) = (x, y + 1); j < map.GetLength(1); j++) //0 +
+            {
+                if (map[i, j] == 'L') break;
+                if (map[i, j] == '#')
+                {
+                    count++; break;
+                }
+            }
+            for (var (i, j) = (x - 1, y + 1); i >= 0 && j < map.GetLength(1); i--, j++)
+            {
+                if (map[i, j] == 'L') break;
+                if (map[i, j] == '#')
+                {
+                    count++; break;
+                }
+            }
+            for (var (i, j) = (x - 1, y); i >= 0 ; i--)
+            {
+                if (map[i, j] == 'L') break;
+                if (map[i, j] == '#')
+                {
+                    count++; break;
+                }
+            }
+            if (map[x, y] == '#')
+            {
+                return count >= 5 ? 'L' : '#';
+            }
+            else
+            {
+                return count == 0 ? '#' : 'L';
+            }
+        }
+
+        public static int Day11()
+        {
+            List<int> adapters = new List<int>();
+            adapters.Add(0); //socket is 0
+            var width = 0;
+            var height = 1;
+            using (Stream stream = File.Open(@"day11", FileMode.Open))
+            using (TextReader sr = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line;
+                line = sr.ReadLine();
+                width = line.Length;
+                while(sr.ReadLine() != null)
+                {
+                    height++;
+                }
+            }
+            char[,] map = new char[width, height];
+            char[,] backup = new char[width, height];
+            using (Stream stream = File.Open(@"day11", FileMode.Open))
+            using (TextReader sr = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line;
+                int i = 0;
+                int j = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    foreach(var c in line)
+                    {
+                        map[i, j] = c;
+                        i++;
+                    }
+                    i = 0;
+                    j++;
+                }
+            }
+
+            bool changed = true;
+            while(changed)
+            {
+                changed = false;
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        backup[i, j] = applyKernel(map, i, j);
+                    }
+                }
+
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        if (map[i, j] != backup[i, j]) changed = true;
+                        map[i, j] = backup[i, j];
+                    }
+                }
+            }
+            int counter = 0;
+            foreach(var c in map)
+            {
+                if (c == '#') counter++;
+            }
+            return counter;
+        }
+
+        public static int Day11Part2()
+        {
+            List<int> adapters = new List<int>();
+            adapters.Add(0); //socket is 0
+            var width = 0;
+            var height = 1;
+            using (Stream stream = File.Open(@"day11", FileMode.Open))
+            using (TextReader sr = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line;
+                line = sr.ReadLine();
+                width = line.Length;
+                while (sr.ReadLine() != null)
+                {
+                    height++;
+                }
+            }
+            char[,] map = new char[width, height];
+            char[,] backup = new char[width, height];
+            using (Stream stream = File.Open(@"day11", FileMode.Open))
+            using (TextReader sr = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line;
+                int i = 0;
+                int j = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    foreach (var c in line)
+                    {
+                        map[i, j] = c;
+                        i++;
+                    }
+                    i = 0;
+                    j++;
+                }
+            }
+
+            bool changed = true;
+            while (changed)
+            {
+                changed = false;
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        backup[i, j] = applyLoS(map, i, j);
+                    }
+                }
+
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        if (map[i, j] != backup[i, j]) changed = true;
+                        map[i, j] = backup[i, j];
+                        //Console.Write(map[i, j]);
+                    }
+                   //Console.WriteLine();
+                }
+                //Console.WriteLine("=========================================================================");
+            }
+            int counter = 0;
+            foreach (var c in map)
+            {
+                if (c == '#') counter++;
+            }
+            return counter;
+        }
 
     }
 }
