c70820b47a38e8476c7a139988beb3f27a32acfc
Henry Oddy-Propsting <h.oddy.propsting@gmail.com>
2020-12-21 20:48:30 +0000

    day 13- I had to look up some help for part 2, I couldn't work out how to use the lcm

--- Program.cs
+++ Program.cs
@@ -36,6 +36,8 @@ namespace AdventOfCode2020
             Console.WriteLine("Day 11 Part 2: {0}", Day11Part2());
             Console.WriteLine("Day 12: {0}", Day12());
             Console.WriteLine("Day 12 Part 2: {0}", Day12Part2());
+            Console.WriteLine("Day 13: {0}", Day13());
+            Console.WriteLine("Day 13 Part 2: {0}", Day13Part2());
         }
         static int Day1()
         {
@@ -1169,6 +1171,79 @@ namespace AdventOfCode2020
             }
 
             return Math.Abs(x) + Math.Abs(y);
+        }
+        public static int Day13()
+        {
+            int time = 0;
+            int bestBus = Int32.MaxValue;
+            Regex busRx = new Regex(@"([0-9]+)");
+            using (Stream stream = File.Open(@"day13", FileMode.Open))
+            using (TextReader sr = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line;
+                line = sr.ReadLine();
+                time = Int32.Parse(line);
+                while ((line = sr.ReadLine()) != null)
+                {
+                    MatchCollection busses = busRx.Matches(line);
+
+                    foreach (Match busMatch in busses)
+                    {
+                        int bus = Int32.Parse(busMatch.Value);
+                        if((bus - (time%bus)) < (bestBus - (time%bestBus)))
+                        {
+                            bestBus = bus;
+                        }
+                    }
+                }
+                var busArrivalTime = ((time / bestBus) + 1) * bestBus;
+                return (busArrivalTime-time) * bestBus;
+            }
+        }
+
+
+        public static BigInteger Day13Part2()
+        {
+            List<(int, int)> busses = new List<(int, int)>();
+            using (Stream stream = File.Open(@"day13", FileMode.Open))
+            using (TextReader sr = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line;
+                line = sr.ReadLine();
+                line = sr.ReadLine();
+                int counter = -1;
+                foreach(var part in line.Split(','))
+                {
+                    counter++;
+                    if (part != "x")
+                    {
+                        busses.Add((Int32.Parse(part), counter));
+                    }
+                }
+            }
+
+            // all the numbers seem to be prime
+            BigInteger time = 0;
+            BigInteger multiple = 0;
+            foreach(var (bus,arrivalTime) in busses)
+            {
+                if(time == 0)
+                {
+                    time = bus;
+                    multiple = bus;
+                } else {
+                    while(true)
+                    {
+                        if((time+arrivalTime) % bus == 0)
+                        {
+                            multiple *= bus;// they are prime so lcm == multiplying all of them
+                            break;
+                        }
+                        time += multiple;
+                    }
+                }
+            }
+            return time;
         }
 
     }
