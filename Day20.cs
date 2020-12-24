90c9270ad8e111c3cc39b760468899c08d608d79
Henry Oddy-Propsting <h.oddy.propsting@gmail.com>
2020-12-24 17:22:48 +0000

    Day 20 part 1

---
+++ Day20.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    class Day20
+    {
+        public static Part1()
+        {
+            List<char[,]> tiles = new List<char[,]>();
+            using (Stream stream = File.Open(@"day20", FileMode.Open))
+            using (TextReader sr = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != "")
+                {}
+            }
+        }
+    }
+}
